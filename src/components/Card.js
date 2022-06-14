72149134fef5b01e22d59f58076107c7489bbe1e
TEST2-jpg <agapdag@gmail.com>
2022-06-14 09:36:21 +0800

    Add shuffle

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,11 +2,25 @@ import React from "react";
 import planets from '../planets'
 
 export default function Places(props) {
-    const planet = planets.data.map(planet => {
+    const [planetData, setPlanet] = React.useState(planets.data)
+
+    function shuffle() {
+        setPlanet(planetData
+            .map(value => ({ value, sort: Math.random() }))
+            .sort((a, b) => a.sort - b.sort)
+            .map(({ value }) => value))
+    }
+
+    React.useEffect(() => {
+        shuffle()
+    }, [])
+
+
+    const planet = planetData.map(planet => {
         return <div className="card" key={planet.id}>
             <div className={planet.name}>
-            <p>{planet.name}</p>
-            <img src={planet.url} className={`${planet.name}--img`}></img>
+                <p className={`${planet.name}--name name`}>{planet.name}</p>
+                <img src={planet.url} className={`${planet.name}--img`} alt={planet.name}></img>
             </div>
         </div>
     })
